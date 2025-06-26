c0130cdef39039aced6e08c828597e9a8f072f52
Vaisakh-Nirupam <vaisakhnirupamvn@gmail.com>
2025-06-27 01:46:18 +0530

    Finished JS

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,2 +1,21 @@
 let inputs = document.getElementById("input");
 let buttons = document.querySelectorAll("button");
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      inputs.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      inputs.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+    } else {
+      string += e.target.innerHTML;
+      inputs.value = string;
+    }
+  });
+});
